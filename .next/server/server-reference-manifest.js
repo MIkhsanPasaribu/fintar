5bbd2e10eb743253d6dd6d4859a86808f52f8c17
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-07-23 13:53:10 +0700

    dump

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"G+AYNzCXGPaTa2q+k6D1ZtnQeycNqM3Va7FO38qP22w=\"\n}"
--- .next/static/chunks/src_app_page_tsx_60426e20._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_e3500ad3._.js"
-],
-    source: "dynamic"
-});
